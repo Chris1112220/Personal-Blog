8d9c56c44e8c1a0fd44eebf38c7c138172fd404b
Chris1112220 <Christopher.roberts11220@gmail.com>
2025-04-06 12:44:18 -0400

    Added branding so I avoid chrome passwrod check

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
 from app import create_app
 from flask_migrate import upgrade
+from flask import request, redirect
 
 app = create_app()
 
+
+@app.before_request
+def enforce_https():
+    if not request.is_secure and not app.debug:
+        url = request.url.replace("http://", "https://", 1)
+        return redirect(url)
+
+
 with app.app_context():
     upgrade()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
